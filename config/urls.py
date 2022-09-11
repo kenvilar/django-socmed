f633434c3fb53e08aca78bf7bee0c20bd3f46f9f
Ken Vilar <kenvilar@gmail.com>
2022-09-11 23:20:40 +0800

    Fix menus

--- config/urls.py
+++ config/urls.py
@@ -9,8 +9,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    # path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path("", TemplateView.as_view(template_name="pages/dashboard.html")),
+    path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
         "dashboard/",
         TemplateView.as_view(template_name="pages/dashboard.html"),
