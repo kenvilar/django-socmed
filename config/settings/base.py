117222bc5733e442482bbd491909c0f225aa37b1
Ken Vilar <kenvilar@gmail.com>
2022-09-11 19:50:31 +0800

    Fix INSTALLED_APPS

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,7 +78,7 @@ THIRD_PARTY_APPS = [
     "drf_spectacular",
 ]
 
-LOCAL_APPS = ["django_socmed.users", "django_socmed.account"]
+LOCAL_APPS = ["django_socmed.users", "django_socmed.accounts"]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
