7caf969e6d21a258c5a439ac141bb1fe315521c6
Ken Vilar <kenvilar@gmail.com>
2022-09-11 23:34:16 +0800

    Change name

--- config/settings/base.py
+++ config/settings/base.py
@@ -314,8 +314,8 @@ CORS_URLS_REGEX = r"^/api/.*$"
 # By Default swagger ui is available only to admin user(s). You can change permission classes to change that
 # See more configuration options at https://drf-spectacular.readthedocs.io/en/latest/settings.html#settings
 SPECTACULAR_SETTINGS = {
-    "TITLE": "Django Socmed API",
-    "DESCRIPTION": "Documentation of API endpoints of Django Socmed",
+    "TITLE": "Bookmark API",
+    "DESCRIPTION": "Documentation of API endpoints of Bookmark",
     "VERSION": "1.0.0",
     "SERVE_PERMISSIONS": ["rest_framework.permissions.IsAdminUser"],
     "SERVERS": [
