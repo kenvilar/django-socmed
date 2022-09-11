95fc2c6d30129022eb02fad6294b5bf4d12d58e3
Ken Vilar <kenvilar@gmail.com>
2022-09-11 23:50:58 +0800

    Use get_user_model to keep code generic

--- django_socmed/accounts/models.py
+++ django_socmed/accounts/models.py
@@ -1,9 +1,11 @@
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True)
 
