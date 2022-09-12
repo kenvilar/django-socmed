a22579b27a9698665ac0cf1e74486fa2abe88cb5
Ken Vilar <kenvilar@gmail.com>
2022-09-12 17:29:39 +0800

    Update Profile models.py

--- django_socmed/accounts/models.py
+++ django_socmed/accounts/models.py
@@ -1,10 +1,11 @@
+from django.conf import settings
 from django.db import models
-
-from django_socmed.users.models import User
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="profile"
+    )
     date_of_birth = models.DateField(blank=True, null=True)
     photo = models.ImageField(upload_to="users/%Y/%m/%d/", blank=True)
 
