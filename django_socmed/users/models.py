5c742a00b2d7cbccfa6c7b58060f14aaeddaeded
Ken Vilar <kenvilar@gmail.com>
2022-09-12 17:03:16 +0800

    add first_name and last_name to User models.py

--- django_socmed/users/models.py
+++ django_socmed/users/models.py
@@ -13,8 +13,8 @@ class User(AbstractUser):
 
     #: First and last name do not cover name patterns around the globe
     name = CharField(_("Name of User"), blank=True, max_length=255)
-    first_name = None  # type: ignore
-    last_name = None  # type: ignore
+    first_name = CharField(_("Firstname of User"), blank=True, max_length=255)
+    last_name = CharField(_("Lastname of User"), blank=True, max_length=255)
 
     def get_absolute_url(self):
         """Get url for user's detail view.
