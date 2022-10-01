51894d9a9278097beb4203d98f5cfe2fdc87e9dc
Ken Vilar <kenvilar@gmail.com>
2022-10-01 17:49:40 +0800

    minor fix

--- django_socmed/images/templates/bookmarklet_launcher.js
+++ django_socmed/images/templates/bookmarklet_launcher.js
@@ -1,9 +1,9 @@
 (function () {
     if (!window.bookmarklet) {
         bookmarklet_js = document.body.appendChild(document.createElement('script'));
-        bookmarklet_js.src = '//localhost:8000/static/js/bookmarklet.js?r=\'+Math.floor(Math.random()*9999999999999999)';
+        bookmarklet_js.src = '//localhost:8000/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 9999999999999999);
         window.bookmarklet = true;
     } else {
         bookmarkletLaunch();
     }
-})
+})();
