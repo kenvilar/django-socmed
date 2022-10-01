490dac60734e50f8715c96c74e23d8d412466ce7
Ken Vilar <kenvilar@gmail.com>
2022-10-01 16:09:21 +0800

    verify that the provided image URL is valid

--- django_socmed/images/forms.py
+++ django_socmed/images/forms.py
@@ -8,3 +8,13 @@ class ImageCreateForm(forms.ModelForm):
         model = Image
         fields = ["title", "url", "description"]
         widgets = {"url": forms.HiddenInput}
+
+    def clean_url(self):
+        url = self.cleaned_data["url"]
+        valid_extensions = ["jpg", "jpeg", "png"]
+        extension = url.rsplit(".", 1)[1].lower()
+        if extension not in valid_extensions:
+            raise forms.ValidationError(
+                "The given URL does not match valid image extensions."
+            )
+        return url
