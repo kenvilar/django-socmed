aaee963e0fc6af83c49b30165e7f66a416c06cbe
Ken Vilar <kenvilar@gmail.com>
2022-10-01 19:25:15 +0800

    add detail image url and get_absolute_url() method to the Image model

--- django_socmed/images/models.py
+++ django_socmed/images/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils.text import slugify
 
 
@@ -32,3 +33,6 @@ class Image(models.Model):
         if not self.slug:
             self.slug = slugify(self.title)
         super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse("images:detail", args=[self.id, self.slug])
