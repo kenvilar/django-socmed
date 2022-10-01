5e4c130929642cb2b3fe126525572d48f9229bcb
Ken Vilar <kenvilar@gmail.com>
2022-10-01 16:39:13 +0800

    Overriding the save() method of a ModelForm

--- django_socmed/images/forms.py
+++ django_socmed/images/forms.py
@@ -1,4 +1,7 @@
+import requests
 from django import forms
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 from .models import Image
 
@@ -18,3 +21,16 @@ class ImageCreateForm(forms.ModelForm):
                 "The given URL does not match valid image extensions."
             )
         return url
+
+    def save(self, force_insert=False, force_update=False, commit=True):
+        image = super().save(commit=False)
+        image_url = self.cleaned_data["url"]
+        name = slugify(image.title)
+        extension = image_url.rsplit(".", 1)[1].lower()
+        image_name = f"{name}.{extension}"
+        # download image from the given URL
+        response = requests.get(image_url)
+        image.image.save(image_name, ContentFile(response.content), save=False)
+        if commit:
+            image.save()
+        return image
