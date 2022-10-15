88733352df2c975a28ee640ce21c196872b8e199
kenvilar <kenvilar@gmail.com>
2022-10-15 23:46:10 +0800

    add new field dynamically to the User model

--- django_socmed/accounts/models.py
+++ django_socmed/accounts/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -30,3 +31,12 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.user_from} follows {self.user_to}"
+
+
+user_model = get_user_model()
+user_model.add_to_class(
+    "following",
+    models.ManyToManyField(
+        "self", through=Contact, related_name="followers", symmetrical=False
+    ),
+)
