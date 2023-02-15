1dba5f8d462730d5194e7a56475808ebd2eba1c1
kenvilar <kenvilar@gmail.com>
2023-02-15 21:29:47 +0800

    change auth.User to settings.AUTH_USER_MODEL

--- django_socmed/actions/models.py
+++ django_socmed/actions/models.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.db import models
 
 
 class Action(models.Model):
     user = models.ForeignKey(
-        "auth.User", related_name="actions", on_delete=models.CASCADE
+        settings.AUTH_USER_MODEL, related_name="actions", on_delete=models.CASCADE
     )
     verb = models.CharField(max_length=255)
     created = models.DateTimeField(auto_now_add=True)
