06f378c02436dc58fb57b8e255f2c3e7f34637bf
kenvilar <kenvilar@gmail.com>
2023-09-25 08:30:39 +0800

    Use http

--- django_socmed/templates/images/bookmarklet_launcher.js
+++ django_socmed/templates/images/bookmarklet_launcher.js
@@ -1,7 +1,7 @@
 (function () {
     if (!window.bookmarklet) {
         bookmarklet_js = document.body.appendChild(document.createElement('script'));
-        bookmarklet_js.src = '//localhost:8000/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 9999999999999999);
+        bookmarklet_js.src = 'http://localhost:8000/static/js/bookmarklet.js?r=' + Math.floor(Math.random() * 9999999999999999);
         window.bookmarklet = true;
     } else {
         bookmarkletLaunch();
