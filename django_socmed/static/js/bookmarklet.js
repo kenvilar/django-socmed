6609459be5ac4417f113d458baad5e4c39ecfe7e
kenvilar <kenvilar@gmail.com>
2023-09-25 08:30:25 +0800

    Use http

--- django_socmed/static/js/bookmarklet.js
+++ django_socmed/static/js/bookmarklet.js
@@ -1,4 +1,4 @@
-const siteUrl = '//localhost:8000/';
+const siteUrl = 'http://localhost:8000/';
 const styleUrl = siteUrl + 'static/css/bookmarklet.css';
 const minWidth = 250;
 const minHeight = 250;
